b10694fb3a81651764ff7a7914c726c72b4dc513
Corliansa Kusumah <corliansa@gmail.com>
2023-05-05 22:21:47 +0200

    fix firebase reinit

--- packages/api/src/firebase.ts
+++ packages/api/src/firebase.ts
@@ -1,10 +1,12 @@
 import admin from "firebase-admin";
-import { initializeApp, type ServiceAccount } from "firebase-admin/app";
+import { type ServiceAccount } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 import serviceAccount from "../service_account.json";
 
-export const adminApp = initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
-});
+export const adminApp = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    })
+  : admin.app();
 export const adminAuth = getAuth(adminApp);
--- packages/auth/src/firebase.ts
+++ packages/auth/src/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,4 +15,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
