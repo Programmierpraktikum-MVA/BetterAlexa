d33a3da875b1d81fc28c7d3c3bcccf5867c66e52
Corliansa Kusumah <corliansa@gmail.com>
2023-05-05 22:21:47 +0200

    fix firebase reinit

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -4,8 +4,7 @@ export const authRouter = createTRPCRouter({
   getSession: publicProcedure.query(({ ctx }) => {
     return ctx.session;
   }),
-  getSecretMessage: protectedProcedure.query(() => {
-    // testing type validation of overridden next-auth Session in @acme/auth package
-    return "you can see this secret message!";
+  getSecretMessage: protectedProcedure.query(({ ctx: { session } }) => {
+    return `you can see this secret message! welcome, ${session.email}`;
   }),
 });
