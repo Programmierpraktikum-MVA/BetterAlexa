fc0662abd718b43d99a6704fc4ce9a4542776773
Corliansa Kusumah <corliansa@gmail.com>
2023-05-06 16:48:02 +0200

    useSession consumes AuthProvider

--- packages/auth/src/useSession.ts
+++ packages/auth/src/useSession.ts
@@ -1,32 +1,5 @@
-import { useEffect, useState } from "react";
-import { onAuthStateChanged, signOut, type User } from "firebase/auth";
+import { useContext } from "react";
 
-import { auth } from "./auth";
+import { AuthContext } from "./AuthProvider";
 
-export interface IUseSession {
-  loading: boolean;
-  user?: User;
-}
-
-const init: IUseSession = {
-  loading: true,
-};
-
-export const useSession = () => {
-  const [session, setSession] = useState(init);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setSession({ loading: false, user: user });
-      } else {
-        void signOut(auth);
-        setSession({ loading: false, user: undefined });
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  return session;
-};
+export const useSession = () => useContext(AuthContext);
