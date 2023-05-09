98a70013bc929ef9e65d35364af8ff1ba94d3e89
Corliansa Kusumah <corliansa@gmail.com>
2023-05-09 23:37:39 +0200

    added sample call to action api

--- apps/sample/api/index.py
+++ apps/sample/api/index.py
@@ -37,5 +37,32 @@ def process_audio():
         return {"error": "Internal Server Error"}, 500
 
 
+@app.route("/call-to-action", methods=["POST"])
+def generate_cta():
+    try:
+        # Check if request method is POST
+        if request.method != "POST":
+            return "Method Not Allowed", 405
+
+        # Parse incoming data as binary
+        data = request.get_data()
+        text = data.decode("utf-8")
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": f"{text}"},
+            ],
+        )
+        result = {
+            "text": response["choices"][0]["message"]["content"],
+        }
+
+        # Respond with success message
+        return {"result": result}, 200
+    except Exception as e:
+        print(e)
+        return {"error": "Internal Server Error"}, 500
+
+
 if __name__ == "__main__":
     app.run(host="::", port=3001, debug=os.environ.get("DEBUG", False))
