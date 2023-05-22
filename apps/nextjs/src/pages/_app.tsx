c4b0a1e9d76b63a2de21d99f90b6e8f9ee12eee2
Corliansa Kusumah <corliansa@gmail.com>
2023-05-22 17:28:42 +0200

    add shadcn toast

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -4,11 +4,13 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 
 import { api } from "~/utils/api";
+import { Toaster } from "~/components/ui/toaster";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <Toaster />
     </AuthProvider>
   );
 };
