c8059dc2af66f0d150dacbd33bd566e029f55034
huju <justin.hulha@campus.tu-berlin.de>
2023-06-08 17:00:44 +0200

    use langchain for c2a

--- apps/command-to-action/api/index.py
+++ apps/command-to-action/api/index.py
@@ -1,41 +1,65 @@
 from flask import Flask, request
-from gtts import gTTS
 
-import openai
-import io
 import os
+from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI, LLMChain
+from langchain.utilities import GoogleSearchAPIWrapper
+from langchain import LLMMathChain, SerpAPIWrapper
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return {"message": "Hello, World!"}
+class LangChain:
+    def __init__(self):
+        llm = OpenAI(temperature=0)
+        llm_math_chain = LLMMathChain(llm=llm)
+        # search = SerpAPIWrapper()
+
+        tools = [
+            # Tool(
+            #     name="Search",
+            #     func=search.run,
+            #     description="useful for when you need to answer questions about current events"
+            # ),
+            Tool(
+                name="Hello_World",
+                func=self.introduction,
+                description="Useful for when you're introducing yourself",
+                return_direct=True
+            ),
+            Tool(
+                name="Custom_Calculator",
+                func=llm_math_chain.run,
+                description="useful for when you need to answer questions about math",
+                return_direct=True
+            )
+        ]
+
+        prefix = """Have a conversation with a human, answering the following questions as best you can. You have access to the following tools:"""
+        suffix = """Begin!"
+
+        {chat_history}
+        Question: {input}
+        {agent_scratchpad}"""
+
+        prompt = ZeroShotAgent.create_prompt(
+            tools,
+            prefix=prefix,
+            suffix=suffix,
+            input_variables=["input", "chat_history", "agent_scratchpad"]
+        )
+        self.memory = ConversationBufferMemory(memory_key="chat_history")
+        self.llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
+        self.agent = ZeroShotAgent(llm_chain=self.llm_chain, tools=tools, verbose=True)
+        self.agent_chain = AgentExecutor.from_agent_and_tools(agent=self.agent, tools=tools, verbose=True,
+                                                              memory=self.memory)
+
+    def introduction(self, name_input):
+        return "Hello " + name_input + ", my name is Jarvis! How can I help you today?"
 
 
-@app.route("/speech-to-text", methods=["POST"])
-def process_audio():
-    try:
-        # Check if request method is POST
-        if request.method != "POST":
-            return "Method Not Allowed", 405
-
-        # Parse incoming data as binary
-        data = request.get_data()
-        audioBlob = bytes(data)
-        audioFile = io.BytesIO(audioBlob)
-        audioFile.name = "audio.wav"  # Add a name attribute to the BytesIO object
-        transcript = openai.Audio.transcribe("whisper-1", audioFile)
-
-        # Here, you can process the audio data as needed. For example, you can
-        # use a third-party library like `ffmpeg` to convert the audio to a
-        # different format, analyze the audio data, etc.
-
-        # Respond with success message
-        return {"result": transcript}, 200
-    except Exception as e:
-        print(e)
-        return {"error": "Internal Server Error"}, 500
+langchain = LangChain()
 
 
 @app.route("/command-to-action", methods=["POST"])
@@ -48,37 +72,13 @@ def generate_cta():
         # Parse incoming data as binary
         data = request.get_data()
         text = data.decode("utf-8")
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": f"{text}"},
-            ],
-        )
+        response = langchain.agent_chain.run(input=text)
         result = {
-            "text": response["choices"][0]["message"]["content"],
+            "text": response,
         }
 
         # Respond with success message
         return {"result": result}, 200
-    except Exception as e:
-        print(e)
-        return {"error": "Internal Server Error"}, 500
-
-
-@app.route("/text-to-speech", methods=["POST"])
-def process_text():
-    try:
-        # Check if request method is POST
-        if request.method != "POST":
-            return "Method Not Allowed", 405
-
-        # Parse incoming data as binary
-        data = request.get_data()
-        text = data.decode("utf-8")
-        tts = gTTS(text=text, lang="en")
-
-        # return tts audio stream
-        return tts.stream()
     except Exception as e:
         print(e)
         return {"error": "Internal Server Error"}, 500
