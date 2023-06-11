64428e84b370dd07b8c0ced908253c9d409204a0
huju <justin.hulha@campus.tu-berlin.de>
2023-06-11 14:43:34 +0200

    extract langchain_integration into its own file

--- apps/command-to-action/api/index.py
+++ apps/command-to-action/api/index.py
@@ -1,65 +1,15 @@
 from flask import Flask, request
-
 import os
-from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
-from langchain.memory import ConversationBufferMemory
-from langchain import OpenAI, LLMChain
-from langchain.utilities import GoogleSearchAPIWrapper
-from langchain import LLMMathChain, SerpAPIWrapper
+
+from langchain_integration import LangChainIntegration
 
 app = Flask(__name__)
 
+langchainIntegration = LangChainIntegration()
 
-class LangChain:
-    def __init__(self):
-        llm = OpenAI(temperature=0)
-        llm_math_chain = LLMMathChain(llm=llm)
-        # search = SerpAPIWrapper()
-
-        tools = [
-            # Tool(
-            #     name="Search",
-            #     func=search.run,
-            #     description="useful for when you need to answer questions about current events"
-            # ),
-            Tool(
-                name="Hello_World",
-                func=self.introduction,
-                description="Useful for when you're introducing yourself",
-                return_direct=True
-            ),
-            Tool(
-                name="Custom_Calculator",
-                func=llm_math_chain.run,
-                description="useful for when you need to answer questions about math",
-                return_direct=True
-            )
-        ]
-
-        prefix = """Have a conversation with a human, answering the following questions as best you can. You have access to the following tools:"""
-        suffix = """Begin!"
-
-        {chat_history}
-        Question: {input}
-        {agent_scratchpad}"""
-
-        prompt = ZeroShotAgent.create_prompt(
-            tools,
-            prefix=prefix,
-            suffix=suffix,
-            input_variables=["input", "chat_history", "agent_scratchpad"]
-        )
-        self.memory = ConversationBufferMemory(memory_key="chat_history")
-        self.llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
-        self.agent = ZeroShotAgent(llm_chain=self.llm_chain, tools=tools, verbose=True)
-        self.agent_chain = AgentExecutor.from_agent_and_tools(agent=self.agent, tools=tools, verbose=True,
-                                                              memory=self.memory)
-
-    def introduction(self, name_input):
-        return "Hello " + name_input + ", my name is Jarvis! How can I help you today?"
-
-
-langchain = LangChain()
+@app.route("/")
+def home():
+    return {"message": "Hello, World!"}
 
 
 @app.route("/command-to-action", methods=["POST"])
@@ -72,7 +22,7 @@ def generate_cta():
         # Parse incoming data as binary
         data = request.get_data()
         text = data.decode("utf-8")
-        response = langchain.agent_chain.run(input=text)
+        response = langchainIntegration.agent_executor.run(input=text)
         result = {
             "text": response,
         }
