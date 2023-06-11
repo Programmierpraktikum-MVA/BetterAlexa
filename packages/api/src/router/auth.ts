b0a11d4b0ed286a9dd6ecd8ae6997cb6840ec571
Corliansa Kusumah <corliansa@gmail.com>
2023-06-11 21:00:32 +0200

    added spotify connect
    Co-authored-by: hu-ju <hu-ju@users.noreply.github.com>

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -4,7 +4,16 @@ export const authRouter = createTRPCRouter({
   getSession: publicProcedure.query(({ ctx }) => {
     return ctx.session;
   }),
-  getSecretMessage: protectedProcedure.query(({ ctx: { session } }) => {
-    return `you can see this secret message! welcome, ${session.email}`;
+  createSpotifyAuth: protectedProcedure.mutation(({ ctx: { session } }) => {
+    const params = new URLSearchParams({
+      response_type: "code",
+      client_id: process.env.SPOTIFY_CLIENT_ID as string,
+      scope:
+        "user-read-private user-modify-playback-state user-read-playback-state user-read-email",
+      redirect_uri: `${process.env.NEXT_PUBLIC_BASE_URL}/api/callback/spotify`,
+      state: session.email as string,
+    });
+
+    return `https://accounts.spotify.com/authorize?${params.toString()}`;
   }),
 });
