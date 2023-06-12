7c5183593a0541c71f45ffc32bce5e6c5f1a0610
Alexey Hramov <alexey.hramov@campus.tu-berlin.de>
2023-06-12 22:47:58 +0200

    bot joins channel and creates ogg files. still many bugs to fix

--- apps/discord/bot.js
+++ apps/discord/bot.js
@@ -1,34 +1,34 @@
 const env = require('dotenv').config({path: '../../.env'});
 const fs = require('fs');
 const Discord = require('discord.js');
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    entersState,
+    StreamType,
+    AudioPlayerStatus,
+    VoiceConnectionStatus,
+    getVoiceConnection
+} = require('@discordjs/voice');
 
 // Check if token was obtained correctly.
 if (env.error) throw env.error;
 
-const textCommandFiles = fs.readdirSync('./text-commands').filter(file => file.endsWith('.js'));
-
-const client = new Discord.Client//();
-    ({
-        intents: [
-            Discord.Intents.FLAGS.GUILDS,
-            Discord.Intents.FLAGS.GUILD_MEMBERS,
-            Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-            Discord.Intents.FLAGS.GUILD_MESSAGES,
-        ],
-    })
-
-// ({
-//     intents: [
-//         Discord.GatewayIntentBits.Guilds,
-//         Discord.GatewayIntentBits.GuildMembers,
-//         Discord.GatewayIntentBits.GuildVoiceStates,
-//         Discord.GatewayIntentBits.GuildMessages,
-//         Discord.GatewayIntentBits.MessageContent,
-//     ],
-// });
-
-client.textCommands = new Discord.Collection();
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES
+    ]
+});
 client.voiceConnections = new Discord.Collection();
+
+const textCommandFiles = fs.readdirSync('./text-commands').filter(file => file.endsWith('.js'));
+client.textCommands = new Discord.Collection();
+
+client.login(env.parsed.DISCORD_TOKEN);
 
 // add text text-commands
 for (const file of textCommandFiles) {
@@ -36,16 +36,14 @@ for (const file of textCommandFiles) {
     client.textCommands.set(command.name, command)
 }
 
-client.once('ready', () => {
-    console.log('BetterAlexa Recognition Bot is Online');
-});
+client.on('ready', () => console.log('BetterAlexa Recognition Bot is Online'));
 
 //TODO warn instead of error for deployment
-client.on('error', (error) => console.error(error));
+client.on('error', console.error);
 
 // Checks for text commands
-client.on('message', message => {
-//client.on('messageCreated', async (message) => {
+client.on('messageCreate', async (message) => {
+    console.log(message.content)
     if (!message.content.startsWith(env.parsed.DISCORD_PREFIX) || message.author.bot) return;
 
     const args = message.content.slice(env.parsed.DISCORD_PREFIX.length).trim().split(' ');
@@ -60,5 +58,3 @@ client.on('voiceStateUpdate', (oldState, newState) => {
         client.textCommands.get('disconnect').execute(newState);
     }
 });
-
-client.login(env.parsed.DISCORD_TOKEN);
--- apps/discord/text-commands/connect.js
+++ apps/discord/text-commands/connect.js
@@ -1,25 +1,26 @@
 const env = require('dotenv').config({path: '../../.env'});
 const {createCommand, fixVoiceReceive, createVoiceConnectionData} = require('../utils.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const {entersState, joinVoiceChannel, VoiceConnection, VoiceConnectionStatus, EndBehaviorType} = require('@discordjs/voice');
 
 if (env.error) throw env.error;
 
 module.exports = createCommand("connect",
     "connect to server and set up streams.",
     async message => {
+        /*
+        * Check if bot has all the permissions it needs to execute the command.
+        */
         const member = message.member;
         const botPermissions = message.channel.permissionsFor(message.client.user);
 
         // Check if bot has permission to message in channel.
-        if (!botPermissions.has("SEND_MESSAGES"))
-        {
+        if (!botPermissions.has("SEND_MESSAGES")) {
             // Message user for sending message access in respective text channel
             return message.channel.send(`I do not have permissions to message in the text channel ${message.channel}. Give me permission or use the command in a different text channel.`);
         }
 
         // Check if there is already a user being listened to (to avoid multiple streams that may break things)
-        if (message.client.voiceConnections.get(message.guild.id))
-        {
+        if (message.client.voiceConnections.get(message.guild.id)) {
             return mess8age.channel.send(`Already listening to <@${message.client.voiceConnections.get(message.guild.id).listeningTo.id}>`)
         }
 
@@ -29,33 +30,76 @@ module.exports = createCommand("connect",
         const voiceChannelPermissions = member.voice.channel.permissionsFor(message.client.user);
 
         // Check if bot has permissions to speak and connection in the user's voice channel
-        if (!voiceChannelPermissions.has("SPEAK") || !voiceChannelPermissions.has("CONNECT"))
-        {
+        if (!voiceChannelPermissions.has("SPEAK") || !voiceChannelPermissions.has("CONNECT")) {
             return message.channel.send(`Can not connect to<@${message.author.id}>'s voice channel.
             I need speak and connect permissions to the voice channel.`);
         }
 
-        const connection = await member.voice.channel.join()
-            // joinVoiceChannel({
-            //     channelId: member.voice.channel.id,
-            //     guildId: message.guild.id,
-            //     adapterCreator: message.guild.voiceAdapterCreator
-            // });
-        //message.channel.send(`Connected to ${member.voice.channel}.`);
 
-        message.channel.send(env.parsed.WARNING_MESSAGE);
+        /*
+        * Create voice connection and set up streams.
+         */
+        const connection =
+            joinVoiceChannel({
+                channelId: member.voice.channel.id,
+                guildId: message.guild.id,
+                adapterCreator: message.guild.voiceAdapterCreator
+            });
+        message.channel.send(`Connected to ${member.voice.channel}.`);
 
-        // play static noise to get voice receive functioning. Some undocumented discord requirement to receive audio
-        fixVoiceReceive(connection);
+        message.channel.send(env.parsed.DISCORD_WARNING_MESSAGE);
 
-        const voiceReceiver = connection.receiver.createStream(member.user,
-            {mode: 'pcm', end: 'manual'});
+        try {
+            await entersState(connection, VoiceConnectionStatus.Ready, 20e3);
+            const receiver = connection.receiver;
 
-        // Make voice streams for voice text-commands
-
-        // Store the connection to the server, the voice recognition to the server, the user to listen to, and the text channel
-        message.client.voiceConnections.set(message.guild.id,
-            createVoiceConnectionData(connection, null, member.user, message.channel));
-
-        console.log(`Guild ${message.guild.id}: created audio stream for ${member.user.username}`);
+            receiver.speaking.on('start', (userId) => {
+                createListeningStream(receiver, userId, message.client.user);
+            });
+        } catch (error) {
+            console.warn(error);
+            message.channel.send('Failed to join voice channel within 20 seconds, please try again later!');
+        }
     });
+
+function getDisplayName(userId, user) {
+    return user ? `${user.username}_${user.discriminator}` : userId;
+}
+
+function createListeningStream(receiver, userId, user) {
+    const {createWriteStream} = require('fs');
+    const {pipeline} = require('stream');
+    const prism = require('prism-media');
+
+    const opusStream = receiver.subscribe(userId, {
+        end: {
+            behavior: EndBehaviorType.AfterSilence,
+            duration: 1000,
+        },
+    });
+
+    const oggStream = new prism.opus.OggLogicalBitstream({
+        opusHead: new prism.opus.OpusHead({
+            channelCount: 2,
+            sampleRate: 48000,
+        }),
+        pageSizeControl: {
+            maxPackets: 10,
+        },
+        crc: false
+    });
+
+    const filename = `./recordings/${Date.now()}-${getDisplayName(userId, user)}.ogg`;
+
+    const out = createWriteStream(filename);
+
+    console.log(`👂 Started recording ${filename}`);
+
+    pipeline(opusStream, oggStream, out, (err) => {
+        if (err) {
+            console.warn(`❌ Error recording file ${filename} - ${err.message}`);
+        } else {
+            console.log(`✅ Recorded ${filename}`);
+        }
+    });
+}
