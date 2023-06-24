3102c41403a8f1952f1416248385d60f0a48f6c3
Corliansa Kusumah <corliansa@gmail.com>
2023-06-24 13:33:24 +0200

    rename _next to assets for extension build

--- apps/nextjs/build-ext.sh
+++ apps/nextjs/build-ext.sh
@@ -3,4 +3,6 @@ sed -i.bak 's/output: "standalone",/output: "export",/g' next.config.mjs
 pnpm build
 mv -f next.config.mjs.bak next.config.mjs
 mkdir -p dist
+mv out/_next out/assets && find out -name '*.html' -exec sed -i.bak 's/\/_next/\/assets/g' {} +
+rm out/*.bak
 zip -r dist/$(date -u +"%Y-%m-%dT%H:%M:%SZ").zip out
