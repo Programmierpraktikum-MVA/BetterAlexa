e607922530e6c9d69091546c3a829f6d3c3032fa
Corliansa Kusumah <corliansa@gmail.com>
2023-06-25 16:23:58 +0200

    fix build extension

--- apps/nextjs/build-ext.sh
+++ apps/nextjs/build-ext.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+rm -rf out
 sed -i.bak 's/output: "standalone",/output: "export",/g' next.config.mjs
 pnpm build
 mv -f next.config.mjs.bak next.config.mjs
+mv out/_next out/assets && find out -type f \( -name '*.html' -o -name '*.js' \) -exec sed -i.bak 's/\/_next/\/assets/g' {} +
+find out -name "*.bak" -type f -delete
 mkdir -p dist
-mv out/_next out/assets && find out -name '*.html' -exec sed -i.bak 's/\/_next/\/assets/g' {} +
-rm out/*.bak
 zip -r dist/$(date -u +"%Y-%m-%dT%H:%M:%SZ").zip out
