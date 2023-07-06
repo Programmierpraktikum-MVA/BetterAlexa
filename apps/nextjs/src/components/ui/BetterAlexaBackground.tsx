9a61d812166681e53d92966d9d5d98485058ccc7
Shin-Young Hur <shin-young.hur@campus.tu-berlin.de>
2023-07-06 18:43:16 +0200

    On Page refresh, ThemeMode is reloaded

--- apps/nextjs/src/components/ui/BetterAlexaBackground.tsx
+++ apps/nextjs/src/components/ui/BetterAlexaBackground.tsx
@@ -1,23 +1,19 @@
 import { type ReactNode } from "react";
-
 import useDarkMode from "./useDarkMode";
 
-export default function BetterAlexaBackground({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  const [colorTheme, setTheme] = useDarkMode();
 
+export default function BetterAlexaBackground({
+  children,}: {children: ReactNode;}) {
+  const [colorTheme, setTheme] = useDarkMode();
+  
   return (
     <>
-      {colorTheme === "dark" ? (
+      {(colorTheme === "light" || colorTheme === undefined || colorTheme === null) ? (
         <main className="flex h-screen flex-col items-center bg-blue-500 font-['Helvetica'] text-sm text-white/100 transition-all duration-1000">
           <div className="fixed left-0 top-0 flex h-16 items-center">
             <button
               className="mx-5 rounded-3xl bg-black/30 px-4  py-2 backdrop-blur-xl hover:bg-black/40 dark:bg-white/20 dark:hover:bg-white/40"
-              onClick={() => setTheme("dark")}
-            >
+              onClick={() => {setTheme("dark")}}>
               Dark mode
             </button>
           </div>
@@ -28,10 +24,11 @@ export default function BetterAlexaBackground({
           <div className="fixed left-0 top-0 flex h-16 items-center">
             <button
               className="mx-5 rounded-3xl bg-black/30 px-4  py-2 backdrop-blur-xl hover:bg-black/40 dark:bg-white/20 dark:hover:bg-white/40"
-              onClick={() => setTheme("light")}
+              onClick={() => {setTheme("light")}}
             >
               Light mode
             </button>
+            
           </div>
           {children}
         </main>
