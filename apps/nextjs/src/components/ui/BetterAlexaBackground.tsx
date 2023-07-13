fa97ea01de8549f982d914a9f4290cb97035b269
antonbadrak <badrak@campus.tu-berlin.de>
2023-07-13 20:12:44 +0200

    fixed background again

--- apps/nextjs/src/components/ui/BetterAlexaBackground.tsx
+++ apps/nextjs/src/components/ui/BetterAlexaBackground.tsx
@@ -7,24 +7,28 @@ export default function BetterAlexaBackground({children} : { children: ReactNode
   return <>
       {colorTheme === "dark"? 
       (
-       <main className="flex h-full flex-col items-center bg-blue-500 font-['Helvetica'] text-sm text-white/100 transition-all duration-1000">
-        <div className="fixed left-0 top-0 flex h-16 items-center">
-          <button className="mx-5 rounded-3xl bg-black/30 dark:bg-white/20  hover:bg-black/40 dark:hover:bg-white/40 px-4 py-2 backdrop-blur-xl" onClick={() =>  setTheme("dark")} >
-            Dark mode
-          </button>
+       <main className="fixed overflow-y-scroll inset-0 h-screen bg-blue-500 transition-all duration-1000">
+        <div className="flex flex-col items-center font-['Helvetica'] text-sm text-white/100">
+          <div className="fixed left-0 top-0 flex h-16 items-center">
+            <button className="mx-5 rounded-3xl bg-black/30 dark:bg-white/20  hover:bg-black/40 dark:hover:bg-white/40 px-4 py-2 backdrop-blur-xl" onClick={() =>  setTheme("dark")} >
+              Dark mode
+            </button>
+          </div>
+          {children}
         </div>
-        {children}
         </main>
         ) 
         : 
         (
-        <main className="flex h-full flex-col items-center bg-gray-900 font-['Helvetica'] text-sm text-white/100 transition-all duration-1000">
-           <div className="fixed left-0 top-0 flex h-16 items-center">
-          <button className="mx-5 rounded-3xl bg-black/30 dark:bg-white/20  hover:bg-black/40 dark:hover:bg-white/40 px-4 py-2 backdrop-blur-xl" onClick={() =>  setTheme("light")} >
-            Light mode
-          </button>
-          </div>
+        <main className="fixed overflow-y-scroll inset-0 h-screen bg-gray-900 transition-all duration-1000">
+          <div className="flex flex-col items-center font-['Helvetica'] text-sm text-white/100">
+            <div className="fixed left-0 top-0 flex h-16 items-center">
+              <button className="mx-5 rounded-3xl bg-black/30 dark:bg-white/20  hover:bg-black/40 dark:hover:bg-white/40 px-4 py-2 backdrop-blur-xl" onClick={() =>  setTheme("light")} >
+                Light mode
+              </button>
+            </div>
           {children}
+          </div>
         </main>
         )}
    
