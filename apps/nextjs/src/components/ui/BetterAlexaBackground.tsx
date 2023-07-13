72d9b3157f1a482c7667151f99d212f2bd1815ca
antonbadrak <badrak@campus.tu-berlin.de>
2023-07-13 20:15:49 +0200

    fixed color animation

--- apps/nextjs/src/components/ui/BetterAlexaBackground.tsx
+++ apps/nextjs/src/components/ui/BetterAlexaBackground.tsx
@@ -7,7 +7,7 @@ export default function BetterAlexaBackground({children} : { children: ReactNode
   return <>
       {colorTheme === "dark"? 
       (
-       <main className="fixed overflow-y-scroll inset-0 h-screen bg-blue-500 transition-all duration-1000">
+       <main className="fixed overflow-y-scroll inset-0 h-screen bg-blue-500 transition-colors duration-1000">
         <div className="flex flex-col items-center font-['Helvetica'] text-sm text-white/100">
           <div className="fixed left-0 top-0 flex h-16 items-center">
             <button className="mx-5 rounded-3xl bg-black/30 dark:bg-white/20  hover:bg-black/40 dark:hover:bg-white/40 px-4 py-2 backdrop-blur-xl" onClick={() =>  setTheme("dark")} >
@@ -20,7 +20,7 @@ export default function BetterAlexaBackground({children} : { children: ReactNode
         ) 
         : 
         (
-        <main className="fixed overflow-y-scroll inset-0 h-screen bg-gray-900 transition-all duration-1000">
+        <main className="fixed overflow-y-scroll inset-0 h-screen bg-gray-900 transition-colors duration-1000">
           <div className="flex flex-col items-center font-['Helvetica'] text-sm text-white/100">
             <div className="fixed left-0 top-0 flex h-16 items-center">
               <button className="mx-5 rounded-3xl bg-black/30 dark:bg-white/20  hover:bg-black/40 dark:hover:bg-white/40 px-4 py-2 backdrop-blur-xl" onClick={() =>  setTheme("light")} >
