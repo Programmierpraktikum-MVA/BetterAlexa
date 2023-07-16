c26f3152540b81c87701827f7bee03cb770511fb
Corliansa Kusumah <corliansa@gmail.com>
2023-07-16 14:18:25 +0200

    changed darkmode button

--- apps/nextjs/src/components/ui/BetterAlexaBackground.tsx
+++ apps/nextjs/src/components/ui/BetterAlexaBackground.tsx
@@ -15,10 +15,10 @@ export default function BetterAlexaBackground({
         <main className="flex h-screen flex-col items-center bg-blue-500 font-['Helvetica'] text-sm text-white/100 transition-all duration-1000">
           <div className="fixed left-0 top-0 flex h-16 items-center">
             <button
-              className="mx-5 rounded-3xl bg-black/30 px-4  py-2 backdrop-blur-xl hover:bg-black/40 dark:bg-white/20 dark:hover:bg-white/40"
+              className="mx-5 h-8 w-8 rounded-3xl bg-black/30 backdrop-blur-xl hover:bg-black/40 dark:bg-white/20 dark:hover:bg-white/40"
               onClick={() => setTheme("dark")}
             >
-              Dark mode
+              ☽
             </button>
           </div>
           {children}
@@ -27,10 +27,10 @@ export default function BetterAlexaBackground({
         <main className="flex h-screen flex-col items-center bg-gray-900 font-['Helvetica'] text-sm text-white/100 transition-all duration-1000">
           <div className="fixed left-0 top-0 flex h-16 items-center">
             <button
-              className="mx-5 rounded-3xl bg-black/30 px-4  py-2 backdrop-blur-xl hover:bg-black/40 dark:bg-white/20 dark:hover:bg-white/40"
+              className="mx-5 h-8 w-8 rounded-3xl bg-black/30 backdrop-blur-xl hover:bg-black/40 dark:bg-white/20 dark:hover:bg-white/40"
               onClick={() => setTheme("light")}
             >
-              Light mode
+              ☀️
             </button>
           </div>
           {children}
