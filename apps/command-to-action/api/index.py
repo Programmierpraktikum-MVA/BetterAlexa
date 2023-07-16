e88e03d0d1c27dfc17b207f370b70afba12f02bb
Corliansa Kusumah <corliansa@gmail.com>
2023-07-16 13:37:53 +0200

    use openai function calling

--- apps/command-to-action/api/index.py
+++ apps/command-to-action/api/index.py
@@ -1,13 +1,72 @@
 from flask import Flask, request
 from waitress import serve
 import sys
-
-from langchain_integration import LangChainIntegration
+import openai
+import json
+import os
+from spotify import SpotifyPlayer
 
 app = Flask(__name__)
 app.logger.setLevel("INFO")
 
-langchainIntegration = LangChainIntegration()
+functions = [
+    {
+        "name": "spotify_player",
+        "description": "Plays a song, artist, album, or playlist on Spotify.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "song_title": {
+                    "type": "string",
+                    "description": "The title of the song to play.",
+                },
+                "artist_name": {
+                    "type": "string",
+                    "description": "The name of the artist to play.",
+                },
+                "album_name": {
+                    "type": "string",
+                    "description": "The name of the album to play.",
+                },
+                "playlist_name": {
+                    "type": "string",
+                    "description": "The name of the playlist to play.",
+                },
+            },
+        },
+    }
+]
+
+
+def spotify_player(args, token):
+    try:
+        spotify_player = SpotifyPlayer(token)
+        if args.get("song_title") and args.get("artist_name"):
+            song_title = args["song_title"]
+            artist_name = args["artist_name"]
+            spotify_player.play_song_from_artist(song_title, artist_name)
+            return f"Playing {song_title} by {artist_name} on Spotify."
+        elif args.get("song_title"):
+            song_title = args["song_title"]
+            spotify_player.play_song(song_title)
+            return f"Playing {song_title} on Spotify."
+        elif args.get("artist_name"):
+            artist_name = args["artist_name"]
+            spotify_player.play_artist(artist_name)
+            return f"Playing {artist_name} on Spotify."
+        elif args.get("album_name"):
+            album_name = args["album_name"]
+            spotify_player.play_album(album_name)
+            return f"Playing {album_name} on Spotify."
+        elif args.get("playlist_name"):
+            playlist_name = args["playlist_name"]
+            spotify_player.play_playlist(playlist_name)
+            return f"Playing {playlist_name} on Spotify."
+        else:
+            return "You need to specify a song, artist, album, or playlist to play on Spotify."
+    except Exception as e:
+        return f"Error: {e}"
+
 
 @app.route("/")
 def home():
@@ -23,11 +82,35 @@ def generate_cta():
 
         # Parse incoming data as binary
         data = request.get_data()
-        langchainIntegration.spotify_auth = request.headers.get("x-spotify-access-token")
         text = data.decode("utf-8")
-        response = langchainIntegration.agent_executor.run(input=text)
+        messages = [{"role": "user", "content": text}]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=messages,
+            functions=functions,
+            function_call="auto",
+        )
+        response_message = response["choices"][0]["message"]
+        app.logger.info(f"Response message: {response_message}")
+
+        if response_message.get("function_call"):
+            function_call = response_message["function_call"]
+            function_name = function_call["name"]
+            arguments = json.loads(function_call["arguments"])
+            if function_name == "spotify_player":
+                if request.headers.get("x-spotify-access-token"):
+                    access_token = request.headers["x-spotify-access-token"]
+                    response_text = spotify_player(arguments, access_token)
+                else:
+                    base_url = os.environ.get("NEXT_PUBLIC_BASE_URL")
+                    response_text = f"You need to authenticate with Spotify first. Go to {base_url}/spotify to do so."
+            else:
+                response_text = "I don't know how to do that yet."
+        else:
+            response_text = response_message["content"]
+
         result = {
-            "text": response,
+            "text": response_text,
         }
 
         # Respond with success message
