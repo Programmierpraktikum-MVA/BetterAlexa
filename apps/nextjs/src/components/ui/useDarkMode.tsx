6a45b1fb82059c1605fea7cee5c329ee89d0b489
Darius3001 <darius.mertin@outlook.com>
2023-07-16 18:37:20 +0200

    fixed build bug (typescript error)

--- apps/nextjs/src/components/ui/useDarkMode.tsx
+++ apps/nextjs/src/components/ui/useDarkMode.tsx
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react";
 
-type Theme = "light" | "dark"| null;
+type Theme = "light" | "dark";
 
-function useDarkMode() {
-  const [theme,setTheme] = useState<Theme>();
+function useDarkMode(): [Theme | undefined, (theme: Theme) => void] {
+  const [theme, setTheme] = useState<Theme>();
   useEffect(() => {
     const currentTheme = window.localStorage.getItem("theme");
-    if(currentTheme !== null && currentTheme !== "undefined" ){
-      setTheme(currentTheme);
+    if (currentTheme !== null && currentTheme !== "undefined") {
+      setTheme(currentTheme as Theme);
     }
-  },[]);
+  }, []);
 
   useEffect(() => {
-    window.localStorage.setItem("theme", theme );
+    window.localStorage.setItem("theme", theme as Theme);
   }, [theme]);
 
   return [theme, setTheme];
 }
 
 export default useDarkMode;
-
-
