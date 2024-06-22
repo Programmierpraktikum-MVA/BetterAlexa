21b7da2eebe2c1c04ded15dae555ab6bcb2e9bf2
Emil Daniel Badura <badura@tu-berlin.de>
2024-06-22 02:50:47 +0200

    download tokenizer without hf token

--- llama3.py
+++ llama3.py
@@ -7,16 +7,20 @@ from trl import setup_chat_format
 
 class LLama3:
     path_to_model: str
+    path_to_tokenizer: str
     model: AutoModelForCausalLM
     tokenizer: AutoTokenizer
     pipeline: Pipeline
     chat: list[dict[str, str]] = []
     chat_length: int = 0
 
-    def __init__(self, destination_path: str, functions: str, drive_link: str | None = None) -> None:
-        self.path_to_model = destination_path
-        if drive_link is not None and not (os.path.exists(destination_path) and os.path.isdir(destination_path)):
-            download_google_drive_folder(drive_link, destination_path)
+    def __init__(self, destination_path: str, functions: str, model_link: str | None = None, tokenizer_link: str | None = None) -> None:
+        self.path_to_model = destination_path + "-model"
+        self.path_to_tokenizer = destination_path + "-tokenizer"
+        if model_link is not None and not (os.path.exists(self.path_to_model) and os.path.isdir(self.path_to_model)):
+            download_google_drive_folder(model_link, self.path_to_model)
+        if tokenizer_link is not None and not (os.path.exists(self.path_to_tokenizer) and os.path.isdir(self.path_to_tokenizer)):
+            download_google_drive_folder(tokenizer_link, self.path_to_tokenizer)
         system_msg = "You are a helpful assistant with access to the following functions. Use them if required -\n{\n" + functions + "\n}"
         self.append_to_chat("system", system_msg)
         self.prepare()
@@ -25,15 +29,12 @@ class LLama3:
         msg = {"role": role, "content": content}
         self.chat.append(msg)
         self.chat_length += len(msg["content"].split())
-        if self.chat_length > 2048:
+        while self.chat_length > 1024:
             msg = self.chat.pop(1)
             self.chat_length -= len(msg["content"].split())
 
     def prepare(self):
-        if os.getenv("HF_TOKEN") is None:
-            print("You need to set the HF_TOKEN environment variable!")
-            quit()
-        tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B', token=os.getenv("HF_TOKEN"))
+        tokenizer = AutoTokenizer.from_pretrained(self.path_to_tokenizer)
         tokenizer.padding_side = "right"
         bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
@@ -57,7 +58,7 @@ class LLama3:
         eos_token_id = self.pipeline.tokenizer.eos_token_id
         pad_token_id = self.pipeline.tokenizer.pad_token_id
 
-        outputs = self.pipeline(prompt, max_new_tokens=256, temperature=0.1, top_k=50, top_p=0.1, eos_token_id=eos_token_id, pad_token_id=pad_token_id)
+        outputs = self.pipeline(prompt, max_new_tokens=512, temperature=0.1, top_k=50, top_p=0.1, eos_token_id=eos_token_id, pad_token_id=pad_token_id)
         response = outputs[0]['generated_text'][len(prompt):].strip()
         self.append_to_chat("assistant", response)
         return response
