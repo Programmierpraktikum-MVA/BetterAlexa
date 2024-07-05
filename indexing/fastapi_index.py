361b81527a23b0144babe26479cf136de64dfae4
PP GPU Server <pp@NEWAdaStruct.AdaStruct.com>
2024-07-05 20:48:40 +0000

    incorporated indexing into the backend architecture

--- indexing/fastapi_index.py
+++ indexing/fastapi_index.py
@@ -3,16 +3,20 @@ from starlette.responses import RedirectResponse
 import uvicorn
 import os
 import loadqdrant
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class UserInput(BaseModel):
+    user_input: str
 
 @app.get("/")
 async def root():
     return RedirectResponse(url="/docs")
-@app.post("/vidindex/{user_input}")
-async def vidindex(user_input: str):
-    response = loadqdrant.queryCollection(user_input, 'MVA')
-    return {'message': response}
+@app.post("/vidindex")
+async def vidindex(input_data: UserInput):
+    output = loadqdrant.queryCollection(input_data.user_input, 'MVA')
+    return {"message": output}
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8047)
+    uvicorn.run(app,host="0.0.0.0", port=8047)
