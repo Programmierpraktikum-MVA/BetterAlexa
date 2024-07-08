03fa71d93edb3e2d264f93897e8d6e4fa77be157
David Gashaw <david.gashaw@bayer.com>
2024-07-08 14:28:11 +0200

    Spotify Button

--- frontend/apps/nextjs/src/pages/spotify.tsx
+++ frontend/apps/nextjs/src/pages/spotify.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { type NextPage } from "next";
-import Head from "next/head";
 import { useRouter } from "next/router";
 
 import { useSession } from "@acme/auth";
@@ -14,59 +13,26 @@ const SpotifyConnect: NextPage = () => {
   const { mutateAsync } = api.auth.createSpotifyAuth.useMutation();
   return (
     <>
-    <RouteGuard/>
-    
-      <Head>
-        <title>BetterAlexa | Connect Spotify</title>
-        <meta
-          name="description"
-          content="BetterAlexa - OpenAI with Langchain Integration"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#2e026d" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Better<span className="text-pink-400">Alexa</span>
-          </h1>
-          {session.loading && <div>Loading...</div>}
-          {!session.loading && !session.user && (
-            <span className="ml-2 text-sm font-semibold text-gray-400">
-              You need to sign in first!
-            </span>
-          )}
-          {session.user && (
-            <button
-              className="rounded-lg bg-gray-700 px-3 py-2 text-sm font-semibold text-white hover:bg-gray-800"
-              onClick={async () => {
-                const url = await mutateAsync();
-                void router.push(url);
-              }}
-            >
-              Connect Spotify
-            </button>
-          )}
-        </div>
-      </main>
+      <RouteGuard />
+
+      <div>
+        {!session.loading && !session.user && (
+          <span className="ml-2 text-sm font-semibold text-gray-400">
+            You need to sign in first!
+          </span>
+        )}
+        {session.user && (
+          <button
+            className="rounded-lg bg-gray-700 px-3 py-2 text-sm font-semibold text-white hover:bg-gray-800"
+            onClick={async () => {
+              const url = await mutateAsync();
+              void router.push(url);
+            }}
+          >
+            Connect Spotify
+          </button>
+        )}
+      </div>
     </>
   );
 };
