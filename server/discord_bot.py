686410b9991249224e3dbec44192eabec175c462
akra05 <kraftadrian171@gmail.com>
2025-05-29 23:02:54 +0200

    discord_bot
    Reads command with syntax: ?join_zoom (zoom meeting link). Returns zoom_link in global variable "zoom_link".

---
+++ server/discord_bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="?", intents=intents)
+
+# Dein Bot-Token hier einfügen:
+TOKEN = "MTM3NzM1NzgwMjQyMjQwMzE3Mg.GVSRjt.RXxG3eCW1-Ri7DXmjm3ROyH_yHwTj5-vRb0c9w"
+
+# Variable, in der der Link gespeichert wird
+zoom_link = None
+
+@bot.event
+async def on_ready():
+    print(f'Bot ist eingeloggt als {bot.user}')
+
+# Command zum Speichern des Zoom-Links
+@bot.command(name="join_zoom")
+async def join_zoom(ctx, link: str):
+    global zoom_link
+    zoom_link = link
+
+    # Speichern des Links in einer Datei (optional, z. B. zum späteren Zugriff)
+    print(zoom_link)
+
+    await ctx.send("Bot tritt dem Zoom-Meeting bei...")
+
+# Bot starten
+bot.run(TOKEN)
