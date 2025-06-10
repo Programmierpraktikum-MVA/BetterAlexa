0351c4bced912e7cfc3073daa593bed36bae677c
smalchen <arne.hoheisel@campus.tu-berlin.de>
2025-06-10 02:07:47 +0200

    working on an embedding of the zoom bot given by timo and integration of discord bot. (WARNING: may not work yet, if there are any problems just comment out the changes)

--- server/discord_bot.py
+++ server/discord_bot.py
@@ -4,33 +4,42 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="?", intents=intents)
+import requests
 
 
-# Dein Bot-Token aus den .env variablen auslesen
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+"""
+This function starts the discord bot named "BetterAlexa" and 
+implements an command for forwarding a zoom invitation link.
+"""
+def start_discord_bot():
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-# Variable, in der der Link gespeichert wird
-zoom_link = None
+    bot = commands.Bot(command_prefix="?", intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Bot ist eingeloggt als {bot.user}')
 
-# Command zum Speichern des Zoom-Links
-@bot.command(name="join_zoom")
-async def join_zoom(ctx, link: str):
-    global zoom_link
-    zoom_link = link
+    # Dein Bot-Token aus den .env variablen auslesen
+    load_dotenv()
+    TOKEN = os.getenv("DISCORD_TOKEN")
 
-    # Speichern des Links in einer Datei (optional, z. B. zum späteren Zugriff)
-    print(zoom_link)
+    # Variable, in der der Link gespeichert wird
+    zoom_link = None
 
-    await ctx.send("Bot tritt dem Zoom-Meeting bei...")
+    @bot.event
+    async def on_ready():
+        print(f'Bot ist eingeloggt als {bot.user}')
 
-# Bot starten
-bot.run(TOKEN)
+    # Command zum Speichern des Zoom-Links
+    @bot.command(name="join_zoom")
+    async def join_zoom(ctx, link: str):
+        global zoom_link
+        zoom_link = link
+
+        # Senden des Zoom links an die fast api
+        url = "http://localhost:8006/handle_zoom_link"  # maybe needs to be adjusted 
+        response = requests.post(url, json={"link": zoom_link})
+
+        await ctx.send("Bot tritt dem Zoom-Meeting bei...")
+
+    # Bot starten
+    bot.run(TOKEN)
