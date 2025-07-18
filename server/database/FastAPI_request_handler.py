b8393bb624bd798f3f3305d92e5201d6476e3706
karl_birke <k.birke.1@campus.tu-berlin.de>
2025-07-18 14:31:22 +0200

    Revert "h"
    This reverts commit 2633e136acbc56cd9add0a341e5d2a4ddf948e80.

--- server/database/FastAPI_request_handler.py
+++ server/database/FastAPI_request_handler.py
@@ -23,7 +23,13 @@ ZOOM_PWD_CACHE  = TTLCache(maxsize=1_000, ttl=PWD_TTL_SECONDS)
 
 router = APIRouter() 
 
-
+router.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # für Tests, später einschränken!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class SettingsPayload(BaseModel):
     user_id: str
